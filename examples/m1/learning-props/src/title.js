d6b10929f1a863009719ca323ff2b0562f1101d9
jrago <#3112Je@>
2019-04-20 02:13:56 -0300

    finalizando a aula 09 do modulo 1 parte 2

--- examples/m1/learning-props/src/title.js
+++ examples/m1/learning-props/src/title.js
@@ -3,9 +3,16 @@
 import React from 'react'
 
 const Title = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'Desconhecido',
+      lastname: 'Sem Sobrenome'
+    }
+  },
+
   render: function() {
     return (
-      <h1>Olá { this.props.name }</h1>
+      <h1>Olá { this.props.name + ' ' + this.props.lastname }!</h1>
     )
   }
 })
