7a41ca594597586a3a2a95829c4d3c525775dab4
jrago <#3112Je@>
2019-04-20 02:51:01 -0300

    finalizando a aula 11 do modulo 1 parte 2

--- examples/m1/learning-props/src/app.js
+++ examples/m1/learning-props/src/app.js
@@ -2,12 +2,15 @@
 
 import  React from 'react'
 import  Title from './title'
+import NewTitle from './newTitle'
+
 
 const App = React.createClass({
   render: function() {
     return (
       <div id='1' data-id='1' className='container' >
         <Title name='Jefferson' lastname={{ first: 'Silva', last: 'Rago'}}/>
+        <NewTitle lastname={{ last: 'Rago'}} />
         <label htmlFor='input'> Input</label>
         <input type='text' id='input' />
       </div>)
