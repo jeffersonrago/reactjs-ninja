cad95dfdc4b5379aa86b04a1608f8f6166aaf196
jrago <#3112Je@>
2019-04-20 02:56:50 -0300

    finalizando a aula 12 do modulo 1 parte 2

--- examples/m1/learning-props/src/app.js
+++ examples/m1/learning-props/src/app.js
@@ -1,20 +1,31 @@
 'use strict'
 
-import  React from 'react'
+import  React, { Component } from 'react'
 import  Title from './title'
 import NewTitle from './newTitle'
 
-
-const App = React.createClass({
-  render: function() {
+ class App extends Component {
+  render () {
     return (
-      <div id='1' data-id='1' className='container' >
+      <div>
         <Title name='Jefferson' lastname={{ first: 'Silva', last: 'Rago'}}/>
         <NewTitle lastname={{ last: 'Rago'}} />
-        <label htmlFor='input'> Input</label>
-        <input type='text' id='input' />
-      </div>)
+      </div>
+    )
   }
-})
+ }
+
+
+// const App = React.createClass({
+//  render: function() {
+// return (
+//      <div id='1' data-id='1' className='container' >
+//        <Title name='Jefferson' lastname={{ first: 'Silva', last: 'Rago'}}/>
+//        <NewTitle lastname={{ last: 'Rago'}} />
+//        <label htmlFor='input'> Input</label>
+//        <input type='text' id='input' />
+//      </div>)
+//  }
+//})
 
 export default App
