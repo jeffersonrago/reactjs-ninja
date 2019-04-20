771799b1c8401ec9e25f6b035f626d41fd5d0eea
jrago <#3112Je@>
2019-04-20 02:05:23 -0300

    finalizando a aula 08 do modulo 1 parte 2

--- examples/m1/learning-props/src/app.js
+++ examples/m1/learning-props/src/app.js
@@ -6,8 +6,10 @@ import  Title from './title'
 const App = React.createClass({
   render: function() {
     return (
-      <div>
+      <div id='1' data-id='1' className='container' >
         <Title name='Jefferson Rago' />
+        <label htmlFor='input'> Input</label>
+        <input type='text' id='input' />
       </div>)
   }
 })
